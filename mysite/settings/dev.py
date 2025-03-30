886e59a250a8a2900d351810c74680193f364e12
Anthony Garrett <tony.garrett@gmail.com>
2025-03-30 17:08:35 -0400

    Initial Commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-lwouiwvk&_#ht*0udn8w!tvgwh(tzh*&d18jzh*#2jpvw!#6e4"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
